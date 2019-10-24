fb9c43c6483abd7a8845ddeba1bbccd0635cba5f
Kim Hoalim <kthoalim@gmail.com>
2019-10-24 15:27:43 -0400

    Added hit, stand and double Buttons

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,27 @@ import Blackjack from "blackjack-dealer-logic"
 
 export default () => {
     alert("It works!");
-    const app =document.getElementById("app");
+    const app = document.getElementById("app");
+    const hitButton = document.getElementById("btn-hit");
+    const standButton = document.getElementById("btn-stand");
+    const doubleButton = document.getElementById("btn-double");
     const blackjack = new Blackjack(app);
+
     blackjack.start();
-}
+
+    hitButton.oncllick = function(){
+        blackjack.hit();
+    }
+
+    standButton.onclick = function(){
+        blackjack.stand();
+    }
+
+    doubleButton.onclick = function(){
+        blackjack.double();
+    }
+
+
+
+   
+};
